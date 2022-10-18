7d9ae3c9f90dc1f2057844c54a33810a9c8dfae3
Pedro Duarte <pduartesilva2005@gmail.com>
2022-10-18 19:37:51 -0300

    Day 01 Completed :rocket:

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436d99", "#2d5752"],
+    mastercard: ["#df6f29", "#c69347"],
+    default: ["black", "gray"]
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+globalThis.setCardType = setCardType
